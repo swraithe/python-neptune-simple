0045078d5af420d9912fd291714b123cab7f138a
swraithe <cutupdev@gmail.com>
2023-08-04 15:07:39 +1000

    from aws code

--- neptune.py
+++ neptune.py
@@ -1,16 +1,15 @@
-from gremlin_python.process.anonymous_traversal import traversal
+from __future__  import print_function  # Python 2/3 compatibility
+
+from gremlin_python import statics
+from gremlin_python.structure.graph import Graph
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.strategies import *
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 
-# Connect to your Neptune cluster
-neptune_endpoint = 'database-neptune.cluster-czmqqwbmeqyz.eu-west-2.neptune.amazonaws.com'
-neptune_port = '8182'
-g = traversal().withRemote(DriverRemoteConnection(f'wss://{neptune_endpoint}:{neptune_port}/gremlin', 'g'))
+graph = Graph()
 
-# Insert nodes node-1, node-2, node-3
-g.addV('node').property('name', 'node-1').property('info', 'Initial info for node-1').as_('node1') \
-    .addV('node').property('name', 'node-2').property('info', 'Initial info for node-2').as_('node2') \
-    .addV('node').property('name', 'node-3').property('info', 'Initial info for node-3').as_('node3') \
-    .iterate()
+remoteConn = DriverRemoteConnection('wss://your-neptune-endpoint:8182/gremlin','g')
+g = graph.traversal().withRemote(remoteConn)
 
-# Commit the changes
-g.tx().commit()
+print(g.V().limit(2).toList())
+remoteConn.close()
