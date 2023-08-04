bffa297bd9ed15b2b01842e1d07045324f888b61
swraithe <cutupdev@gmail.com>
2023-08-04 16:21:49 +1000

    added graph node

--- neptune.py
+++ neptune.py
@@ -10,6 +10,14 @@ graph = Graph()
 
 remoteConn = DriverRemoteConnection('wss://database-neptune-instance-1.czmqqwbmeqyz.eu-west-2.neptune.amazonaws.com:8182/gremlin','g')
 g = graph.traversal().withRemote(remoteConn)
+# Insert nodes node-1, node-2, node-3
+g.addV('node').property('name', 'node-1').property('info', 'Initial info for node-1').as_('node1') \
+    .addV('node').property('name', 'node-2').property('info', 'Initial info for node-2').as_('node2') \
+    .addV('node').property('name', 'node-3').property('info', 'Initial info for node-3').as_('node3') \
+    .iterate()
+
+# Commit the changes
+g.tx().commit()
 
 print(g.V().limit(2).toList())
 remoteConn.close()
